72234b0de697c84fd24b9c6dd2308df998b39f32
Alex Anderson <aanderson65@wisc.edu>
2020-01-10 15:34:33 -0600

    Add sudo permission to agent 2 script

--- scripts/agent/2-node.bash
+++ scripts/agent/2-node.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash - && \
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add - && \
-wget -qO- https://deb.opera.com/archive.key | apt-key add - && \
-echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+wget -qO- https://deb.opera.com/archive.key | sudo apt-key add -
+sudo touch /etc/apt/sources.list.d/google.list
+echo "deb http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee -a /etc/apt/sources.list.d/google.list
