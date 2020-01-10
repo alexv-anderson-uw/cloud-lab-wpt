847fa705138b46b4749da472d9ded945bc5c3fd0
Alex Anderson <aanderson65@wisc.edu>
2020-01-10 15:35:28 -0600

    Add sudo permission to 3rd agent script

--- scripts/agent/3-apt-repo-add.bash
+++ scripts/agent/3-apt-repo-add.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-add-apt-repository -y ppa:ubuntu-mozilla-daily/ppa
-add-apt-repository -y 'deb https://deb.opera.com/opera-stable/ stable non-free'
-add-apt-repository -y 'deb https://deb.opera.com/opera-beta/ stable non-free'
-add-apt-repository -y 'deb https://deb.opera.com/opera-developer/ stable non-free'
+sudo add-apt-repository -y ppa:ubuntu-mozilla-daily/ppa
+sudo add-apt-repository -y 'deb https://deb.opera.com/opera-stable/ stable non-free'
+sudo add-apt-repository -y 'deb https://deb.opera.com/opera-beta/ stable non-free'
+sudo add-apt-repository -y 'deb https://deb.opera.com/opera-developer/ stable non-free'
