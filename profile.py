73c9c28b639e384a5a8b2f5c5f980de17d6afa9a
Alex Anderson <aanderson65@wisc.edu>
2020-01-10 13:57:55 -0600

    Skip user group for PHP configuration

--- profile.py
+++ profile.py
@@ -14,6 +14,8 @@ node = request.XenVM("node")
 
 # Ubuntu 16.04 LTS 64-bit
 node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU16-64-STD"
+
+# Get an IP address for the VM (CloudLab Docs 10.3: https://docs.cloudlab.us/advanced-topics.html)
 node.routable_control_ip = True
 
 portal.context.printRequestRSpec()
--- steps/5 php.bash
+++ steps/5 php.bash
@@ -4,5 +4,5 @@ cd ~
 
 PHPVER=$(find /etc/php/7.* -maxdepth 0 -type d -printf '%f')
 cat wptserver-install/configs/php/php.ini | sudo tee /etc/php/$PHPVER/fpm/php.ini
-cat wptserver-install/configs/php/pool.www.conf | sed "s/%USER%/$USER/" | sudo tee /etc/php/$PHPVER/fpm/pool.d/www.conf
+cat wptserver-install/configs/php/pool.www.conf | sed "s/%USER%/$USER/" | sed "s/.*group.*$USER//" | sudo tee /etc/php/$PHPVER/fpm/pool.d/www.conf
 sudo service php$PHPVER-fpm restart
